a4845db35cc13444438ab54255cea97628955adc
Cenk Sevimli <cenksml@hotmail.com>
2017-06-02 13:48:23 +0200

    new info in

--- src/Chapter6/MaximumFinder.java
+++ src/Chapter6/MaximumFinder.java
@@ -2,7 +2,7 @@ package Chapter6;
 
 import java.util.Scanner;
 
-// executed and tried git
+// executed and tried git.
 //Programmer-declared method maximum with three double parameters.
 public class MaximumFinder
 {
