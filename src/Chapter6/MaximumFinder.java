aa3efcfcd0d9fc60c9d9b36334daf4b8d2d6821b
Cenk Sevimli <cenksml@hotmail.com>
2017-06-02 14:16:11 +0200

    all deleted

--- src/Chapter6/MaximumFinder.java
+++ src/Chapter6/MaximumFinder.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MaximumFinder
 {
- // make new change
+ // make new changee
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
