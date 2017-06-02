6af26364abb73d70d7fa47072a664273236bc608
Cenk Sevimli <cenksml@hotmail.com>
2017-06-02 14:09:22 +0200

    deleted

--- src/Chapter6/MaximumFinder.java
+++ src/Chapter6/MaximumFinder.java
@@ -1,7 +1,7 @@
 package Chapter6;
 
 import java.util.Scanner;
-//executed and tried git
+
 // executed and tried git.
 //Programmer-declared method maximum with three double parameters.
 public class MaximumFinder
