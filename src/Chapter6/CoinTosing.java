4bcb7c2d6f808bd59053826704a8d68bed09ae29
Cenk Sevimli <cenksml@hotmail.com>
2017-06-05 11:26:57 +0200

    CoinTosing

--- src/Chapter6/CoinTosing.java
+++ src/Chapter6/CoinTosing.java
@@ -1,42 +1,76 @@
 package Chapter6;
 
+/** ANMERKUNG :// soruda buradan bisey dönmesini istiyor ben öyle yapmadým
+ * @author cenks BU PROGRAM BÝTMEDÝ ENUM FALN YAPADIM TEKRAR YAP
+ *
+ */
+
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class CoinTosing 
 {
 	private static final SecureRandom randomNumbers = new SecureRandom();
 	
 	private enum Coin {HEADS, TAILS};
+	
+	private static Coin side;
 
 	public static void main(String[] args) 
-	{
-		Coin side;
-		int head = 0;
+	{	
+		int heads = 0;
 		int tail = 0;
+		boolean flag = true;
 		
-		int a = flip();
+		Scanner input = new Scanner(System.in);
 		
-		if(a==0) 
+		System.out.println("Play the game");
+		System.out.printf("%nenter 1 for play OR 0 for exit:");
+		
+		while(flag) 
 		{
-			side = Coin.HEADS;
-			++head;
-		}			
-		else
-		{
-			side = Coin.TAILS;
-			++tail;
+			int decide = input.nextInt();
 			
-		}
+			if (decide == 0)
+			{
+				System.out.println();
+				System.out.println("--------------------------------------");
+				System.out.printf("total Heads: %d%ntotal Tails: %d%n",
+						heads, tail);
+				System.out.println("--------------------------------------");
+				flag =false;	
+			}				
+			else 				
+			{
+				flip();
 				
-
+				if(side == Coin .HEADS) 
+				{
+					++heads;
+					System.out.printf("it is heads%n");
+				}			
+				else
+				{
+					++tail;
+					System.out.printf("it is tail%n");
+				}
+				
+				System.out.printf("%nenter 1 to continue OR 0 for exit:");
+			}
+		}
+		
+		System.out.printf("%nyou left the game!!!");
 	}
 	
-	public static int flip()
+	public static void flip() // soruda buradan bisey dönmesini istiyor ben öyle yapmadým
 	{
 		int binary = randomNumbers.nextInt(2);
-		
-		return binary;
-	} 
 	
+		if(binary ==0)	
+			side = Coin.HEADS;
 
+		if(binary ==1)
+			 side = Coin.TAILS;
+
+	}
 }
