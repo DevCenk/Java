2f9e1f7f98187e57ab36727281ea6f708b1bf5a1
Cenk Sevimli <cenksml@hotmail.com>
2017-06-05 22:30:01 +0200

    good info for try-catch

--- src/Chapter7/StudentPoll.java
+++ src/Chapter7/StudentPoll.java
@@ -10,6 +10,9 @@ package Chapter7;
 	//Java’s exception-handling mechanism, which can be used to detect and
 	//handle an ArrayIndexOutOfBoundsException.
 
+	// Exception handling helps you create fault-tolerant programs that can resolve (or handle) exceptions
+
+
 public class StudentPoll 
 {
 	public static void main(String[] args)
@@ -18,7 +21,7 @@ public class StudentPoll
 		int[] responses = { 1, 2, 5, 4, 3, 5, 2, 1, 3, 3, 1, 4, 3, 3, 3,
 				2, 3, 3, 2, 14 };
 		
-		int[] frequency = new int[6];  // array of frequency counters // 5 i göreblmek için 6 ya kadar uzatýyo
+		int[] frequency = new int[6];  // array of frequency counters // 5 i görebilmek için 6 ya kadar uzatýyor.
 		// better than switch
 		//  Each element is used as a counter for one of the possible types of survey responses—
 		
@@ -26,14 +29,28 @@ public class StudentPoll
 		{
 			try
 			{
-				++frequency[responses[answer]];	 
+				++frequency[responses[answer]];	 //  that line throws an
+												//  ArrayIndexOutOfBoundsException to notify 
+												// the program of this problem and that catch handles that
 			}
 			catch (ArrayIndexOutOfBoundsException e) 
 			{
 				System.out.println(e); // invokes toString method
 				System.out.printf(" responses[%d] = %d%n%n",
 						 answer, responses[answer]); 
-			}			
+			}	
+			
+			// To handle an exception, place any code that might throw an exception in a try statement.
+			// The try block contains the code that might throw an exception,
+			// and the catch block contains the code that handles the exception if one occurs.	
+			
+			// When " ++frequency[responses[answer]]; "correctly increments an element
+			// of the frequency array, then catch block is ignored
+			
+			// implicitly calls the exception object’s toString method to
+			// get the error message that’s implicitly stored in the exception object and display it. Once
+			// the message is displayed in this example, the exception is considered handled and the program
+			// continues with the next statement after the catch block’s closing brace
 		}
 		
 		System.out.printf("%s%10s%n", "Rating", "Frequency");
